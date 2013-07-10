726a8c2cee2dbf03cca891bd37715ad7a421c9f5
John Smith <example@stanford.edu>
2013-07-10 09:06:42 +0000

    test

---
+++ web.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+   
+var express = require('express');
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+    var my_index = fs.readFileSync("index.html");
+  response.send(my_index.toString('utf-8'));
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+}); 
